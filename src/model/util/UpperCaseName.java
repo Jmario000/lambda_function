60cd6ffa0ef694e0a609317f8d48a9dfc85ed5a7
José Mário Neto <jose.mario40@outlook.com>
2024-11-24 14:05:28 -0300

    Function implementation

---
+++ src/model/util/UpperCaseName.java
@@ -0,0 +1,14 @@
+package model.util;
+
+import java.util.function.Function;
+
+import model.entities.Product;
+
+public class UpperCaseName implements Function<Product, String> {
+
+	@Override
+	public String apply(Product t) {
+		return t.getName().toUpperCase();
+	}
+
+}
